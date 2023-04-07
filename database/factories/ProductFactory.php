0db81868b274c5c2b821129f2d2aecd8d95452e7
Victor Lelis <ovictorlelis@gmail.com>
2023-04-07 11:08:23 -0300

    adicionado migrations / seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'cover' => $this->faker->imageUrl,
+            'price' => $this->faker->randomFloat(2, 0, 100),
+            'description' => $this->faker->sentence,
+            'stock' => $this->faker->randomDigit,
+        ];
+    }
+}
